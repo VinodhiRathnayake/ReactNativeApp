0a89d165cc454fa4317baf1e50e3dfc1e0ce4a63
Vinodhi Rathnayake <vinodhirathnayake30@gmail.com>
2024-01-25 21:13:04 +0530

    commit

--- App.js
+++ App.js
@@ -1,18 +1,14 @@
 import React from "react";
-import { TextInput, View, Text } from "react-native";
-import Screen from "./app/components/Screen";
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/ListItem";
-import AccountScreen from "./app/screens/AccountScreen";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import ListingScreen from "./app/screens/ListingScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
 
 
 export default function App() {
  return(
-  <Screen>
-    <TextInput placeholder="FirstName"/>
-    <Text> hiii</Text>
-  </Screen>
+ <MessagesScreen />
 
  );
 }
